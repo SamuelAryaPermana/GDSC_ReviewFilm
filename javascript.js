0fe33e48981544a5611a8005ac7034abcbb8b385
SamuelAryaPermana <arya20samuel@gmail.com>
2021-10-29 20:53:43 +0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var tombolhome = $(".tombol-menu")
+ var menu = $(".nav .menu ul");
+
+ function klikMenu(){
+     tombolhome.click(function () {
+         menu.toggle();
+         
+     })
+     menu.click(function () {
+         menu.toggle
+     })
+ }
+$(document).ready(function () {
+var width = $(window).width();
+
+})
